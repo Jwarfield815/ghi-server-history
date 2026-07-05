77c9d39abfd556f53a37485e856243bf35ac8606
Jwarfield815 <jonathanwarfield@gmail.com>
2026-07-05 14:30:05 -0500

    replaced placeholder text on the takes flight page

---
+++ servers/1211_taking_flight/images/ls_json.php
@@ -0,0 +1,14 @@
+<?php
+
+// grab the files
+$files = scandir(dirname(__FILE__));
+
+// remove "." and ".." (and anything else you might not want)
+$output = [];
+foreach ($files as $file)
+  if (!in_array($file, [".", "..", "thumbs"]))
+    $output[] = $file;
+
+// out we go
+header("Content-type: application/json");
+echo json_encode($output);
